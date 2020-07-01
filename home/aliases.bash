0f7ce92ef32baa1effcf1fdca7bf0981e4c28ee4
Alex Vzorov <alex.vzorov@gmail.com>
2020-07-01 19:20:34 +0800

    Update dotfiles

--- home/aliases.bash
+++ home/aliases.bash
@@ -1,89 +1,86 @@
-## .zshrc.local
+# aliases.bash
 
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
-export PATH=$PATH:/Applications/WebKit.app/Contents/Frameworks/10.9/JavaScriptCore.framework/Resources
-export PATH=$PATH:/usr/local/share/npm/bin
-export PATH=$PATH:$CLOJURESCRIPT_HOME/bin
-export PATH=/usr/local/heroku/bin:$PATH
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin
-# export PATH="$PATH:$(yarn global bin)"
-export PATH="/usr/local/heroku/bin:$PATH"
-export JRUBY_OPTS="-Xcompile.invokedynamic=false -J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1 -J-noverify -Xcompile.mode=OFF"
-export ANDROID_HOME=$HOME/code/android/sdk
-export GOPATH=$HOME/code/go
-export GROOVY_HOME=/usr/local/opt/groovy/libexec
-export MONO_GAC_PREFIX="/usr/local"
-export TODO_TXT_PATH=$HOME/.todo.txt
-export XML_CATALOG_FILES=/usr/local/etc/xml/catalog
-export V8_HOME=/usr/local/Cellar/v8/3.21.17/bin
-export MAKEFLAGS="-j 8"
-export MY_RUBY_HOME=/usr
-export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
-# export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
-
-if which brew > /dev/null; then
-  export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
-fi
-
-eval "$(fasd --init auto)"
-
-alias bx='bundle exec'
 alias bo='bundle open'
+alias bx='bundle exec'
+alias c=cli
 alias cb='`git rev-parse --abbrev-ref HEAD`'
-alias gpu='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
+alias code.="code ."
+alias dns='sudo dnsmasq -k'
+alias e.='e .'
+alias e=$EDITOR
+alias ea="$EDITOR ~/.aliases.bash"
+alias eal="$EDITOR ~/.config/alacritty/alacritty.yml"
+alias ec="$EDITOR ~/.lein/profiles.clj"
+alias ec="$EDITOR ~/.local/bin/cli"
+alias ei="$EDITOR ~/.inputrc"
+alias el="$EDITOR ~/Documents/Ledger/2018.ledger"
+alias ep="$EDITOR ~/.bash_profile"
+alias es="$EDITOR ~/.ssh/config"
+alias et="$EDITOR ~/.tmux.conf"
+alias ev="$EDITOR ~/.vimrc"
 alias g='git status'
-alias gco='git checkout'
-alias gd='git diff'
-alias gp='git push'
-alias gf='git fetch'
-alias gfa='git fetch --all'
 alias gb='git branch -v'
+alias gbf='gbr | fzf | xargs git checkout'
 alias gbi='git branch | cut -c 3- | fzf | xargs git checkout'
-alias gmi='git branch | cut -c 3- | fzf | xargs -I {} git merge {} --no-ff'
+alias gbr='git for-each-ref --count=15 --sort=-committerdate refs/heads/ --format="%(refname:short)"'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gco='git checkout'
+alias gd='git diff'
+alias gf='git fetch'
+alias gfa='git fetch --all'
 alias gff='git merge --ff-only origin/`git rev-parse --abbrev-ref HEAD`'
+alias gfff='gf && gff'
+alias git=hub
 alias gl='git log --graph --oneline --decorate'
-
+alias gmi='git branch | cut -c 3- | fzf | xargs -I {} git merge {} --no-ff'
+alias gp='git push'
+alias gpu='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
+alias idea='open -a "IntelliJ IDEA CE"'
+alias lent='lentil . -a ex%rb -x test -f comp'
+alias lr='ledger -f ~/Documents/Ledger/2018.ledger'
+alias mps="mix phx.server"
+alias mt="mix test"
+alias mf="mix test --failed"
+alias ng='ngrok tls -region=ap -hostname="*.${BC_DEV_DOMAIN}" 4443'
+alias nowatch="export BC_DEV_NO_WATCH_ASSETS=1"
 alias o='open'
-alias reload='source ~/.zprofile'
-alias ra='source ~/.aliases.zsh'
-
-alias e=$EDITOR
-alias ec="$EDITOR ~/.lein/profiles.clj"
-alias ea="$EDITOR ~/.aliases.zsh"
-alias ev="$EDITOR ~/.vimrc"
-alias ep="$EDITOR ~/.zprofile"
-alias el="$EDITOR ~/.zsh.local"
-alias er="$EDITOR ~/.zshrc"
-alias ei="$EDITOR ~/.inputrc"
-alias et="$EDITOR ~/.tmux.conf"
-alias es="$EDITOR ~/.ssh/config"
-alias eal="$EDITOR ~/.config/alacritty/alacritty.yml"
-
-# osx
-alias el="$EDITOR ~/Documents/Ledger/money1.ledger"
-alias lr='ledger -f ~/Documents/Ledger/money1.ledger'
+alias pgup="postgres -D /usr/local/var/postgres10.1"
+alias ra='source ~/.aliases.bash'
+alias rp='source ~/.bash_profile'
+alias st.='open -a SourceTree .'
+alias st='open -a SourceTree'
 alias tailf='tail -f'
+alias uf='(cd ../.. && mix format)'
+alias xee='open -a "Xee³"'
+alias time='/usr/bin/time'
+alias commit="vim :Git"
+alias mecast="sudo ifconfig en0 ether f4:f5:d8:0f:e6:ae"
+alias meme="sudo ifconfig en0 ether 80:35:c1:35:6d:cb"
+alias nocast="sudo ifconfig en0 ether 28:cf:e9:19:a0:9f"
+alias r=ranger
+alias sbf="stack build --fast"
+alias restart_camera="sudo killall VDCAssistant"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias sleepless="pmset -g assertions | egrep '(PreventUserIdleSystemSleep|PreventUserIdleDisplaySleep)'"
+alias post="curl -X POST"
+alias vim=nvim
+alias gvim=neovide
+alias testmobi="source .mobi && stack exec bluecode-test --"
+alias testmobir="stack build --fast && source .mobi && stack exec bluecode-test --"
 
-tmuxstart() {
-  TMUX= tmux -2 new-session -d -s $1
-  TMUX= tmux send-keys -t "$1" "$2" C-m
+function youtube-au() {
+  youtube-dl -x "$1" --audio-format mp3
 }
 
-start-kafka() {
-  tmux new-session -d -s kafka-server -n zookeeper-server zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties
-  tmux new-window -d -n kafka-server -t kafka-server kafka-server-start /usr/local/etc/kafka/server.properties
+function dark() {
+  export DARK_MODE=$(osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode=true")
 }
 
-stop-kafka() {
-  kafka-server-stop; zookeeper-server-stop
-}
-
-function tm {
-  tmux -2 new-session -s "$USER"
+function light() {
+  export DARK_MODE=$(osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode=false")
 }
 
 # fj - changing directory with fasd
@@ -96,17 +93,6 @@ function clip() {
   [ -t 0 ] && pbpaste || pbcopy
 }
 
-alias c=cli
-alias ec="vim ~/.local/bin/cli"
-alias e.='e .'
-alias st='open -a SourceTree'
-alias st.='open -a SourceTree .'
-alias git=hub
-alias mps="mix phx.server"
-alias vim=nvim
-alias xee='open -a "Xee³"'
-alias idea='open -a "IntelliJ IDEA CE"'
-
 function gcoi() {
   git checkout `git branch --list --no-color | cut -c 3- | fzf -q $1`
 }
@@ -115,5 +101,7 @@ function gbdi() { # git branch -D interactively
   git branch -d `git branch --list --no-color | cut -c 3- | fzf -q $1`
 }
 
-alias pgup="postgres -D /usr/local/var/postgres10.1"
+function recode() {
+  ffmpeg -i "$1" "_$1"
+}
 
